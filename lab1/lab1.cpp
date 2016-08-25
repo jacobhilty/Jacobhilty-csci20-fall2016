5bf9b170baae9b5dffadb4c098f00c788b199530
Jacob Hilty <jhilty001@student.butte.edu>
2016-08-25 20:58:58 +0000

    ASCII Art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,14 @@
+//jacob hilty
+//8/25/16
+//Creating ASCII art
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<" |    |   -----     -----  -----  -----  -----  |    |  -----  ----- "<<endl;
+    cout<<" |    |     |         |    |      |   |  |      |    |  |      |    | "<<endl;
+    cout<<" |----|     |         |    |---   |---|  |      |----|  |---   |------"<<endl;
+    cout<<" |    |     |         |    |      |   |  |      |    |  |      |      | "<<endl;
+    cout<<" |    |   -----       |    -----  |   |  -----  |    |  -----  |      | "<<endl;
+}
